b04f6fed7462c4d3ece96c34952468396c5e137e
supportqc <116244041+supportqc@users.noreply.github.com>
2023-02-23 20:20:45 -0500

    evan commit 1

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import styles from "./style.js";
+
 export default class Header extends React.Component {
   render() {
     return (
