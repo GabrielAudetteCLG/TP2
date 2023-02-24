6c4a595252cec0d90d536aa4ac0a999f3d8131c2
supportqc <116244041+supportqc@users.noreply.github.com>
2023-02-24 10:55:07 -0500

    flatlist

--- components/produit.js
+++ components/produit.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { View, Text, Image, Pressable } from "react-native";
-import image from "../assets/tshirt.png";
+import { View, Text, Image, Pressable, FlatList } from "react-native";
 import styles from "./style.js";
 import listeProduit from "../Data/listeProduit";
 let updatedListeProduit;
@@ -13,32 +12,34 @@ export default class Produit extends React.Component {
     console.log(listeProduit);
     console.log(`updated ${updatedListeProduit}`);
   };
-  render() {
-    return (
-      <>
-        {listeProduit.map((item) => (
-          <View style={styles.produitContainer} key={item.id}>
-            <View style={styles.imgContainer}>
-              <Image style={styles.img} source={{ uri: item.imageProduit }} />
-            </View>
-            <View style={styles.produitInfoContainer}>
-              <View style={styles.produitTextContainer}>
-                <Text style={styles.produitTexte}>{item.nomProduit}</Text>
-                <Text style={styles.produitTexte}>{item.prixProduit}$</Text>
-              </View>
-              <Pressable
-                style={styles.buttonContainer}
-                onPress={() => {
-                  this.deleteItem(item.id);
-                  this.setState({ listeProduit: updatedListeProduit });
-                }}
-              >
-                <Text style={styles.buttonText}>X</Text>
-              </Pressable>
-            </View>
+render() {
+  return (
+    <FlatList
+      data={listeProduit}
+      renderItem={({ item }) => (
+        <View style={styles.produitContainer} key={item.id}>
+          <View style={styles.imgContainer}>
+            <Image style={styles.img} source={{ uri: item.imageProduit }} />
           </View>
-        ))}
-      </>
-    );
-  }
+          <View style={styles.produitInfoContainer}>
+            <View style={styles.produitTextContainer}>
+              <Text style={styles.produitTexte}>{item.nomProduit}</Text>
+              <Text style={styles.produitTexte}>{item.prixProduit}$</Text>
+            </View>
+            <Pressable
+              style={styles.buttonContainer}
+              onPress={() => {
+                this.deleteItem(item.id);
+                this.setState({ listeProduit: updatedListeProduit });
+              }}
+            >
+              <Text style={styles.buttonText}>X</Text>
+            </Pressable>
+          </View>
+        </View>
+      )}
+      keyExtractor={(item) => item.id}
+    />
+  );
+}
 }
